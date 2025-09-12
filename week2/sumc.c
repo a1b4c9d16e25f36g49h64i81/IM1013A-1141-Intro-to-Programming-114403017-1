c6ffe8ffd3212ab59ebce3afef9192e37c57a621
a1b4c9d16e25f36g49h64i81 <131430382+a1b4c9d16e25f36g49h64i81@users.noreply.github.com>
2025-09-12 02:51:07 +0000

    update sumc

---
+++ week2/sumc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    int integert1;
+    int integert2;
+    
+    printf("enter your integert\n");
+    scanf("%d",&integert1);
+
+    printf("enter your integert\n");
+    scanf("%d",&integert2);
+
+    int sum;
+    sum=integert1+integert2;
+    printf("the sum is %d",sum)
+}
